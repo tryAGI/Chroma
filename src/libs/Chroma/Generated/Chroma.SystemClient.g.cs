91f590b43b6026901be1834b61eccc8fa325b16e
Konstantin S. <havendv@gmail.com>
2026-09-13 21:57:27 +0400

    feat: Updated OpenAPI spec (#157)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Chroma/Generated/Chroma.SystemClient.g.cs
+++ src/libs/Chroma/Generated/Chroma.SystemClient.g.cs
@@ -34,7 +34,7 @@ namespace Chroma
         /// <inheritdoc/>
         public global::Chroma.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Chroma.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Chroma.SystemSourceGenerationContext.Default);
 
         /// <summary>
         ///
